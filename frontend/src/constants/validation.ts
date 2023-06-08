b69a772cab9510c9e2f6d15b22dd80cab08678d6
NguyenCatTuong <nct542@gmail.com>
2023-06-08 23:15:13 +0700

    🐞 fix(frontend): fE: using react hook form

--- frontend/src/constants/validation.ts
+++ frontend/src/constants/validation.ts
@@ -2,5 +2,11 @@ export const MAX = {
   PRODUCT_NAME: 200,
   CATEGORY_NAME: 100,
   PRODUCT_PRICE: 1_000_000_000,
-  PRODUCT_UNIT: 50
+  PRODUCT_UNIT: 50,
+  CUSTOMER_NAME: 100
+};
+
+export const REGEX = {
+  PHONE_NUMBER:
+    /^((\\+[1-9]{1,4}[ \\-]*)|(\\([0-9]{2,3}\\)[ \\-]*)|([0-9]{2,4})[ \\-]*)*?[0-9]{3,4}?[ \\-]*[0-9]{3,4}?$/
 };
